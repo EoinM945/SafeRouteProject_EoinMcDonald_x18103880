a24a1b28b2b17fc60671daecc544ff3285b8a282
EoinM945 <Smiley10494@gmail.com>
2021-12-22 20:56:11 +0000

    Initial commit

---
+++ app/src/main/java/com/example/saferouteproject_eoinmcdonald_x18103880/Register.java
@@ -0,0 +1,102 @@
+package com.example.saferouteproject_eoinmcdonald_x18103880;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class Register extends AppCompatActivity {
+
+    final DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://saferoute-9e40c-default-rtdb.firebaseio.com/");
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_register);
+
+        final EditText fullname  = findViewById(R.id.fullname);
+        final EditText email = findViewById(R.id.email);
+        final EditText phone = findViewById(R.id.phone);
+        final EditText password = findViewById(R.id.password);
+        final EditText conPassword = findViewById(R.id.conPassword);
+
+        final Button registerBtn = findViewById(R.id.registerBtn);
+        final TextView loginNowBtn = findViewById(R.id.loginNowBtn);
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final String fullnameTxt = fullname.getText().toString();
+                final String emailTxt = email.getText().toString();
+                final String phoneTxt = phone.getText().toString();
+                final String passwordTxt = password.getText().toString();
+                final String conPasswordTxt = conPassword.getText().toString();
+
+                if(fullnameTxt.isEmpty() || emailTxt.isEmpty() || phoneTxt.isEmpty() || passwordTxt.isEmpty() || conPasswordTxt.isEmpty()){
+                    Toast.makeText(Register.this, "Please fill in all fields.", Toast.LENGTH_SHORT).show();
+                }
+                else if(!passwordTxt.equals(conPasswordTxt)){
+                    Toast.makeText(Register.this, "Passwords Do Not Match", Toast.LENGTH_SHORT).show();
+                }
+                else{
+
+                    databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                            if(snapshot.hasChild(emailTxt)){
+                                Toast.makeText(Register.this, "Email already registered", Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                databaseReference.child("users").child(emailTxt).child("fullname").setValue(fullnameTxt);
+                                databaseReference.child("users").child(emailTxt).child("phone").setValue(phoneTxt);
+                                databaseReference.child("users").child(emailTxt).child("password").setValue(passwordTxt);
+
+                                Toast.makeText(Register.this, "Registration Successfull", Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+
+
+
+
+                }
+
+
+            }
+        });
+
+
+        loginNowBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/saferouteproject_eoinmcdonald_x18103880/UserProfile.java
@@ -0,0 +1,23 @@
+package com.example.saferouteproject_eoinmcdonald_x18103880;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class UserProfile extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_user_profile);
+
+
+    }
+}
