973134d173e98478abf4d051810815a3841d4edc
EoinM945 <smiley10494@gmail.com>
2022-05-01 15:59:22 +0100

    Traffic cameras added more

--- app/src/main/java/com/example/saferouteproject_eoinmcdonald_x18103880/MapsActivity.kt
+++ app/src/main/java/com/example/saferouteproject_eoinmcdonald_x18103880/MapsActivity.kt
@@ -60,7 +60,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback
 
     lateinit var B_search: Button
 
-    lateinit var tvCurrentAddress: TextView
+    private lateinit var tvCurrentAddress: TextView
 
     private var fusedLocationProviderClient: FusedLocationProviderClient? = null
 
@@ -221,6 +221,162 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback
         latlngs.add(LatLng(53.279522,-6.317178))
         latlngs.add(LatLng(53.291406,-6.171467))
         latlngs.add(LatLng(53.275318,-6.344791))
+        latlngs.add(LatLng(53.348142,-6.405587))
+        latlngs.add(LatLng(53.287941,-6.326264))
+        latlngs.add(LatLng(53.297878,-6.28673))
+        latlngs.add(LatLng(53.337783,-6.265855))
+        latlngs.add(LatLng(53.344177,-6.264451))
+        latlngs.add(LatLng(53.288168,-6.291097))
+        latlngs.add(LatLng(53.232871,-6.199448))
+        latlngs.add(LatLng(53.291367,-6.245667))
+        latlngs.add(LatLng(53.292739,-6.246345))
+        latlngs.add(LatLng(53.313099,-6.247058))
+        latlngs.add(LatLng(53.289535,-6.129546))
+        latlngs.add(LatLng(53.341765,-6.252578))
+        latlngs.add(LatLng(53.342151,-6.2505))
+        latlngs.add(LatLng(53.345611,-6.257516))
+        latlngs.add(LatLng(53.255708,-6.126384))
+        latlngs.add(LatLng(53.278077,-6.106968))
+        latlngs.add(LatLng(53.287805,-6.125867))
+        latlngs.add(LatLng(53.334943,-6.454122))
+        latlngs.add(LatLng(53.377064,-6.439811))
+        latlngs.add(LatLng(53.393526,-6.398195))
+        latlngs.add(LatLng(53.396868,-6.392844))
+        latlngs.add(LatLng(53.400533,-6.38081))
+        latlngs.add(LatLng(53.595686,-6.263354))
+        latlngs.add(LatLng(53.606937,-6.20252))
+        latlngs.add(LatLng(53.447226,-6.220453))
+        latlngs.add(LatLng(53.46505,-6.21566))
+        latlngs.add(LatLng(53.489034,-6.312472))
+        latlngs.add(LatLng(53.482713,-6.182694))
+        latlngs.add(LatLng(53.552344,-6.102111))
+        latlngs.add(LatLng(53.571322,-6.100446))
+        latlngs.add(LatLng(53.336443,-6.217618))
+        latlngs.add(LatLng(53.352514,-6.224988))
+        latlngs.add(LatLng(53.360331,-6.238465))
+        latlngs.add(LatLng(53.361384,-6.259737))
+        latlngs.add(LatLng(53.382882,-6.244191))
+        latlngs.add(LatLng(53.387691,-6.244323))
+        latlngs.add(LatLng(53.403799,-6.240473))
+        latlngs.add(LatLng(53.453881,-6.214771))
+        latlngs.add(LatLng(53.46491,-6.21489))
+        latlngs.add(LatLng(53.508464,-6.192774))
+        latlngs.add(LatLng(53.558024,-6.211155))
+        latlngs.add(LatLng(53.630168,-6.2129))
+        latlngs.add(LatLng(53.284439,-6.225361))
+        latlngs.add(LatLng(53.320621,-6.421514))
+        latlngs.add(LatLng(53.326048,-6.358127))
+        latlngs.add(LatLng(53.328182,-6.38038))
+        latlngs.add(LatLng(53.356017,-6.26526))
+        latlngs.add(LatLng(53.421299,-6.324857))
+        latlngs.add(LatLng(53.29211,-6.406868))
+        latlngs.add(LatLng(53.303301,-6.419881))
+        latlngs.add(LatLng(53.299248,-6.300862))
+        latlngs.add(LatLng(53.339299,-6.27259))
+        latlngs.add(LatLng(53.34287,-6.271843))
+        latlngs.add(LatLng(53.344285,-6.261846))
+        latlngs.add(LatLng(53.331977,-6.251448))
+        latlngs.add(LatLng(53.338366,-6.255585))
+        latlngs.add(LatLng(53.341799,-6.258115))
+        latlngs.add(LatLng(53.345322,-6.25846))
+        latlngs.add(LatLng(53.410436,-6.215934))
+        latlngs.add(LatLng(53.409973,-6.22565))
+        latlngs.add(LatLng(53.37225,-6.331975))
+        latlngs.add(LatLng(53.615937,-6.637757))
+        latlngs.add(LatLng(53.350766,-6.356233))
+        latlngs.add(LatLng(53.346781,-6.227618))
+        latlngs.add(LatLng(53.343527,-6.246426))
+        latlngs.add(LatLng(53.34828,-6.254945))
+        latlngs.add(LatLng(53.343527,-6.246426))
+        latlngs.add(LatLng(53.359035,-6.262042))
+        latlngs.add(LatLng(53.354198,-6.266939))
+        latlngs.add(LatLng(53.337184,-6.286463))
+        latlngs.add(LatLng(53.360857,-6.296334))
+        latlngs.add(LatLng(53.362388,-6.298298))
+        latlngs.add(LatLng(53.370202,-6.345294))
+        latlngs.add(LatLng(53.373254,-6.165499))
+        latlngs.add(LatLng(53.370365,-6.19435))
+        latlngs.add(LatLng(53.377793,-6.172549))
+        latlngs.add(LatLng(53.391103,-6.167679))
+        latlngs.add(LatLng(53.318847,-6.36553))
+        latlngs.add(LatLng(53.332597,-6.26486))
+        latlngs.add(LatLng(53.336483,-6.298985))
+        latlngs.add(LatLng(53.334122,-6.327773))
+        latlngs.add(LatLng(53.342746,-6.241154))
+        latlngs.add(LatLng(53.329528,-6.232014))
+        latlngs.add(LatLng(53.337539,-6.251445))
+        latlngs.add(LatLng(53.30244,-6.308956))
+        latlngs.add(LatLng(53.312673,-6.303218))
+        latlngs.add(LatLng(53.321471,-6.265828))
+        latlngs.add(LatLng(53.291908,-6.265104))
+        latlngs.add(LatLng(53.281542,-6.278538))
+        latlngs.add(LatLng(53.290225,-6.192352))
+        latlngs.add(LatLng(53.299472,-6.179711))
+        latlngs.add(LatLng(53.292811,-6.229536))
+        latlngs.add(LatLng(53.29152,-6.233469))
+        latlngs.add(LatLng(53.285253,-6.160581))
+        latlngs.add(LatLng(53.27989,-6.13539))
+        latlngs.add(LatLng(53.28856,-6.145505))
+        latlngs.add(LatLng(53.343374,-6.335259))
+        latlngs.add(LatLng(53.347121,-6.384617))
+        latlngs.add(LatLng(53.34783,-6.392978))
+        latlngs.add(LatLng(53.357885,-6.234219))
+        latlngs.add(LatLng(53.354569,-6.467205))
+        latlngs.add(LatLng(53.465024,-6.216148))
+        latlngs.add(LatLng(53.292583,-6.400451))
+        latlngs.add(LatLng(53.305353,-6.360867))
+        latlngs.add(LatLng(53.385918,-6.396457))
+        latlngs.add(LatLng(53.267808,-6.160819))
+        latlngs.add(LatLng(53.563001,-6.443354))
+        latlngs.add(LatLng(53.380991,-6.358063))
+        latlngs.add(LatLng(53.282397,-6.193279))
+        latlngs.add(LatLng(53.295783,-6.145739))
+        latlngs.add(LatLng(53.357951,-6.474633))
+        latlngs.add(LatLng(53.357836,-6.406334))
+        latlngs.add(LatLng(53.355661,-6.397584))
+        latlngs.add(LatLng(53.304399,-6.413305))
+        latlngs.add(LatLng(53.284065,-6.381751))
+        latlngs.add(LatLng(53.578109,-6.210705))
+        latlngs.add(LatLng(53.498632,-6.417299))
+        latlngs.add(LatLng(53.408673,-6.227987))
+        latlngs.add(LatLng(53.401438,-6.407482))
+        latlngs.add(LatLng(53.339548,-6.275368))
+        latlngs.add(LatLng(53.288103,-6.196004))
+        latlngs.add(LatLng(53.339486,-6.309938))
+        latlngs.add(LatLng(53.381131,-6.058269))
+        latlngs.add(LatLng(53.273019,-6.309066))
+        latlngs.add(LatLng(53.357898,-6.356705))
+        latlngs.add(LatLng(53.279696,-6.136744))
+        latlngs.add(LatLng(53.350552,-6.279736))
+        latlngs.add(LatLng(53.339778,-6.252038))
+        latlngs.add(LatLng(53.339778,-6.252038))
+        latlngs.add(LatLng(53.286922,-6.266193))
+        latlngs.add(LatLng(53.32986,-6.284341))
+        latlngs.add(LatLng(53.341027,-6.231696))
+        latlngs.add(LatLng(53.322031,-6.294872))
+        latlngs.add(LatLng(53.377669,-6.276928))
+        latlngs.add(LatLng(53.45433,-6.215489))
+        latlngs.add(LatLng(53.324883,-6.299314))
+        latlngs.add(LatLng(53.353728,-6.254723))
+        latlngs.add(LatLng(53.473407,-6.221376))
+        latlngs.add(LatLng(53.319832,-6.291236))
+        latlngs.add(LatLng(53.337386,-6.262768))
+        latlngs.add(LatLng(53.346327,-6.243533))
+        latlngs.add(LatLng(53.285592,-6.407888))
+        latlngs.add(LatLng(53.343127,-6.269914))
+        latlngs.add(LatLng(53.347716,-6.259278))
+        latlngs.add(LatLng(53.402575,-6.291566))
+        latlngs.add(LatLng(53.323364,-6.207084))
+        latlngs.add(LatLng(53.380245,-6.210704))
+        latlngs.add(LatLng(53.33929,-6.354948))
+        latlngs.add(LatLng(53.287453,-6.246474))
+        latlngs.add(LatLng(53.387746,-6.32044))
+        latlngs.add(LatLng(53.272953,-6.124833))
+        latlngs.add(LatLng(53.427074,-6.243001))
+        latlngs.add(LatLng(53.351504,-6.258752))
+        latlngs.add(LatLng(53.265442,-6.235214))
+        latlngs.add(LatLng(53.386048,-6.295778))
+        latlngs.add(LatLng(53.406863,-6.397759))
 
         for (point in latlngs) {
             options.position(point)
@@ -234,7 +390,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback
         googleMap.addMarker(
             MarkerOptions()
                 .position(traffic1)
-                .title("Marker in Sydney")
+                .title("Marker")
         )*/
 
 
@@ -290,7 +446,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback
 
         B_search.setOnClickListener {
 
-            searchArea()
+            this.searchArea()
         }
 
     }
@@ -357,11 +513,32 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback
                     ).show()
 
 
-                    tvCurrentAddress!!.setText("Distance = $s KM")
+                    tvCurrentAddress.run {
+
+
+                        //Setting marker to draw route between these two points
+                        origin = MarkerOptions().position(LatLng(latitude, longitude))
+                                        .title("HSR Layout").snippet("origin")
+                        destination =
+                                        MarkerOptions().position(LatLng(end_latitude, end_longitude))
+                                            .title(tf_location.text.toString())
+                                            .snippet("Distance = $s KM")
+                        mMap!!.addMarker(destination)
+                        mMap!!.addMarker(origin)
+
+                        Toast.makeText(
+                                        this@MapsActivity,
+                                        "Distance = $s KM",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+
+
+                        setText("Distance = $s KM")
+                    }
 
                     //getting URL to the google Directions API
                     val url: String =
-                        getDirectionsUrl(origin!!.getPosition(), destination!!.getPosition())!!
+                        getDirectionsUrl(origin.getPosition(), destination.getPosition())!!
 
                     val downloadTask = DownloadTask()
 
@@ -375,7 +552,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback
 
 
     @Throws(IOException::class)
-    private fun downloadUrl(strUrl: String): String? {
+    private fun downloadUrl(strUrl: String): String {
         var data = ""
         var iStream: InputStream? = null
         var urlConnection: HttpURLConnection? = null
@@ -383,7 +560,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback
             val url = URL(strUrl)
             urlConnection = url.openConnection() as HttpURLConnection
             urlConnection.connect()
-            iStream = urlConnection!!.inputStream
+            iStream = urlConnection.inputStream
             val br =
                 BufferedReader(InputStreamReader(iStream))
             val sb = StringBuffer()
@@ -443,7 +620,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback
                 lineOptions.geodesic(true)
             }
 
-            if (points.size != 0) mMap!!.addPolyline(lineOptions)
+            if (points.size != 0) {
+                mMap!!.addPolyline(lineOptions)
+            }
         }
 
     }
@@ -473,7 +652,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback
     }
 
 
-    private fun getDirectionsUrl(origin: LatLng, dest: LatLng): String? {
+    private fun getDirectionsUrl(origin: LatLng, dest: LatLng): String {
         //Origin of route
         val str_origin = "origin=" + origin.latitude + "," + origin.longitude
 
